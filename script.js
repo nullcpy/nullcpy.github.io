224960bbfc6b38084fe586a872358d0d30771fee
nullcpy <213671755+nullcpy@users.noreply.github.com>
2026-03-25 07:46:42 +0000

    de-revanced support added

--- script.js
+++ script.js
@@ -1207,7 +1207,7 @@ function formatBrandDisplayName(value) {
     const brandOverrides = {
         youtube: 'YouTube', revanced: 'ReVanced', tiktok: 'TikTok', soundcloud: 'SoundCloud',
         vpn: 'VPN', reddit: 'Reddit', instagram: 'Instagram', twitter: 'Twitter', rvx: 'ReVanced Extended',
-        anddea: 'ReVanced Advanced', exp: 'Experimental', mocha: 'Mocha Theme', nord: 'Nord Theme', materialu: 'Material You'
+        anddea: 'ReVanced Advanced', exp: 'Experimental', mocha: 'Mocha Theme', nord: 'Nord Theme', materialu: 'Material You', derv: "De-ReVanced"
     };
     return toTitleWords(value)
         .split(' ')
